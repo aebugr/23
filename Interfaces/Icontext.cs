b96ca7f1505a02544c5594c043081ab70a396ee4
User <User@DESKTOP-LE23F7N>
2023-12-13 21:30:50 +0500

    Интерфейсы созданы. Бугрина

---
+++ Interfaces/Icontext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingGiftsBugrina.Interfaces
+{
+    public interface Icontext
+    {
+        void Save(bool Update = false);
+        List<Classes.OrderContext> All();
+        void Delete();
+    }
+}
