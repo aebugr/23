0afa4f139a82514583fec3381f3c347118ecc981
User <User@DESKTOP-LE23F7N>
2023-12-13 21:29:50 +0500

    Модели созданы. Бугрина

---
+++ Models/OrderInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingGiftsBugrina.Models
+{
+    public class OrderInfo
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public string Message { get; set; }
+        public string Adress { get; set; }
+        public DateTime Date { get; set; }
+        public string Mail { get; set; }
+    }
+}
