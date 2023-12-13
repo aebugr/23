bfe8394872e8782f23ba14815b2ff68b0cf0573c
User <User@DESKTOP-LE23F7N>
2023-12-13 23:10:50 +0500

    Страница Main создана. Бугрина

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,47 @@
+﻿using OrderingGiftsBugrina.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderingGiftsBugrina.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreateUI();
+        }
+        public void CreateUI()
+        {
+            parent.Children.Clear();
+            foreach (OrderContext order in MainWindow.init.All)
+            {
+                parent.Children.Add(new Elements.ItemOrder(order));
+            }
+        }
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+
+        private void AddDocuments(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.add);
+        }
+    }
+}
